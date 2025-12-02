8d2023d67e3058f162761cb44cbfcb73a94912e6
Vitor Santos <vitor57960646@aluno.ms.senac.br>
2025-12-02 17:28:53 -0400

    Exercicio 2 feito

---
+++ 50 Exercicios/Exercicio2.py
@@ -0,0 +1,13 @@
+#Leia três números reais e determine qual deles é o maior. Em caso de igualdade, informe adequadamente.
+num = []
+valores = 0
+for i in range(3):
+    num.append(int(input(f"Digite o {i + 1}° numero: ")))
+
+for i in range(len(num)):
+    if max(num) == num[i]:
+        valores += 1
+
+print(f"Maior numero: {max(num)}")
+
+print(f"Numeros repetidos: {valores}") if valores > 1 else print("Não tem numeros repetidos")
